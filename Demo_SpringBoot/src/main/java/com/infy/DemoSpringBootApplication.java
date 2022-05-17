17adb5935e499c166f5fafb6d698d24863906ab5
vishn <vishn@192.168.0.103>
2022-05-17 23:49:56 +0530

    vnnvvn

--- Demo_SpringBoot/src/main/java/com/infy/DemoSpringBootApplication.java
+++ Demo_SpringBoot/src/main/java/com/infy/DemoSpringBootApplication.java
@@ -9,7 +9,7 @@ public class DemoSpringBootApplication {
 	public static void main(String[] args) {
 		
 		SpringApplication.run(DemoSpringBootApplication.class, args);
-		System.out.println("welcome to boot");
+		System.out.println("welcome to boot");//
 	
 	}
 
