ade3e61390d9df578e98fda48cb263da7a8a3b7e
vishn <vishn@192.168.0.103>
2022-05-17 22:00:28 +0530

    demo first push

---
+++ Demo_SpringBoot/src/main/java/com/infy/DemoSpringBootApplication.java
@@ -0,0 +1,16 @@
+package com.infy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(DemoSpringBootApplication.class, args);
+		System.out.println("welcome to boot");
+	
+	}
+
+}
